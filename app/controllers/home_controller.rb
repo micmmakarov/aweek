5f7f2bd08906e3f4e6e19e3fe1a8fa2c7f6804d8
Michael makarov <info1117@gmail.com>
2012-04-10 10:33:32 -0700

    ok

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
   skip_before_filter :authenticate_user!
   
   def index
-	@new_post = Post.new
     @posts = Post.all
     @events = Event.all 
     @organisations = Organisation.all 
