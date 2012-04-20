0b3f373e78c15d6e7f6eaab2ddf0eac6a78e8118
Michael makarov <info1117@gmail.com>
2012-04-20 13:39:14 -0700

    !

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -3,13 +3,13 @@ class Attachment < ActiveRecord::Base
 	belongs_to :post
 	belongs_to :event
 	
-	has_attached_file :pic, :styles => { :medium => "500x280>", :thumb => "150x100#" },
+	has_attached_file :pic, :styles => { :medium => "500x280>",:slide => "180x260>", :thumb => "150x100#" },
 	:storage => :s3,
     :bucket => 'aweek1024',
     :s3_credentials => {
       :access_key_id => 'AKIAI25BIECHIO4NCM3Q',
       :secret_access_key => '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',
-    },
-	:path => ":attachment/:id/:style.:extension"
+    }
+	#:path => ":attachment/:id/:style.:extension"
 
 end
