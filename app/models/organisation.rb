8b0cdc6940fdc8e032aa10cbf0a6da26bfcf1fa3
Michael makarov <info1117@gmail.com>
2012-04-22 12:32:26 -0700

    s3 to organisations

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,6 +1,13 @@
 class Organisation < ActiveRecord::Base
 
-  has_attached_file :pic, :styles => { :medium => "x300", :thumb => "60x60#" }
+  has_attached_file :pic, :styles => { :medium => "x300", :thumb => "60x60#" },
+  :storage => :s3,
+    :bucket => 'aweek1024',
+	:convert_options => { :thumb => "-quality 92" },
+    :s3_credentials => {
+      :access_key_id => 'AKIAI25BIECHIO4NCM3Q',
+      :secret_access_key => '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',
+    }
 	
   belongs_to :user
   has_many :posts
