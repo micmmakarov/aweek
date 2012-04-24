5cf2c88f2263fab25f4fc3d34a9c2b34ef539c46
Michael makarov <info1117@gmail.com>
2012-04-23 18:11:56 -0700

    new thumb

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -3,7 +3,7 @@ class Attachment < ActiveRecord::Base
 	belongs_to :post
 	belongs_to :event
 	
-	has_attached_file :pic, :styles => { :medium => "500x",:slide => "180x260#", :thumb => "150x100#" },
+	has_attached_file :pic, :styles => { :feed => "400x400>", :medium => "500x500>",:slide => "180x260#", :thumb => "150x100#" },
 	:storage => :s3,
     :bucket => 'aweek1024',
 	:convert_options => { :thumb => "-quality 92" },
