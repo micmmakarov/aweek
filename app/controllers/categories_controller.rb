6539a8d31c54f09128ef9973870be30148b21db7
Michael makarov <info1117@gmail.com>
2012-04-25 16:36:12 -0700

    comments

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,6 +15,7 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
 	@posts = @category.posts
+	@events = @category.events
 	
     respond_to do |format|
       format.html # show.html.erb
