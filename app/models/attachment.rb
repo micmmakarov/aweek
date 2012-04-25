7af5095ce03304fc052b2e7aca178b5f6f767894
Michael makarov <info1117@gmail.com>
2012-04-25 14:37:29 -0700

    height

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -3,7 +3,7 @@ class Attachment < ActiveRecord::Base
 	belongs_to :post
 	belongs_to :event
 	
-	has_attached_file :pic, :styles => { :feed => "400x400>", :medium => "500x500>",:slide => "180x260#", :thumb => "150x100#" },
+	has_attached_file :pic, :styles => { :feed => "480x400>", :medium => "500x500>",:slide => "180x260#", :thumb => "150x100#" },
 	:storage => :s3,
     :bucket => 'aweek1024',
 	:convert_options => { :thumb => "-quality 92" },
@@ -12,5 +12,20 @@ class Attachment < ActiveRecord::Base
       :secret_access_key => 'F0tuvY4NladtIxwfWoSckvvWYwNWV9A2AvIW5j+A',
     }
 	#:path => ":attachment/:id/:style.:extension"
-
+	
+	
+  def upload_image(url)
+    begin
+      io = open(URI.escape(url))
+      if io
+        def io.original_filename; base_uri.path.split('/').last; end
+        io.original_filename.blank? ? nil : io
+        p.pic = io
+      end
+      p.save(false)
+    rescue Exception => e
+      logger.info "EXCEPTION# #{e.message}"
+    end
+  end
+	
 end
